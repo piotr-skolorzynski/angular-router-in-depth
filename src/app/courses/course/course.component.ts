6d113cd81f8990b78ef8fe938555688cb165e511
piotr-skolorzynski <p.skolorzynski@gmail.com>
2024-01-13 13:13:59 +0100

    router resolver service

--- src/app/courses/course/course.component.ts
+++ src/app/courses/course/course.component.ts
@@ -1,4 +1,5 @@
-import { Component, OnInit } from "@angular/core";
+import { Component, OnInit, inject } from "@angular/core";
+import { ActivatedRoute } from "@angular/router";
 import { Course } from "../model/course";
 
 @Component({
@@ -8,10 +9,11 @@ import { Course } from "../model/course";
 })
 export class CourseComponent implements OnInit {
   course: Course;
-
   couponCode: string;
+  private readonly route = inject(ActivatedRoute);
 
-  constructor() {}
-
-  ngOnInit() {}
+  ngOnInit() {
+    //course data will be provided by CourseResolver and we need to store it
+    this.course = this.route.snapshot.data["course"];
+  }
 }
--- src/app/courses/courses-routing.module.ts
+++ src/app/courses/courses-routing.module.ts
@@ -2,6 +2,7 @@ import { NgModule } from "@angular/core";
 import { Routes, RouterModule } from "@angular/router";
 import { HomeComponent } from "./home/home.component";
 import { CourseComponent } from "./course/course.component";
+import { CourseResolver } from "./services/course.resolver";
 
 const routes: Routes = [
   {
@@ -11,12 +12,14 @@ const routes: Routes = [
   {
     path: ":courseUrl",
     component: CourseComponent,
+    //means that before showing CourseComponent we need to load course data using CourseResolver
+    resolve: { course: CourseResolver },
   },
 ];
 
 @NgModule({
   imports: [RouterModule.forChild(routes)],
   exports: [RouterModule],
-  providers: [],
+  providers: [CourseResolver],
 })
 export class CoursesRoutingModule {}
