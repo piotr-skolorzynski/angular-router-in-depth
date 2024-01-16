fb1b5d693b327be268db64301594b5b566f1cc34
piotr-skolorzynski <p.skolorzynski@gmail.com>
2024-01-16 23:12:16 +0100

    router secondary outlet

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -10,6 +10,7 @@ import { AboutComponent } from "./about/about.component";
 import { PageNotFoundComponent } from "./page-not-found/page-not-found.component";
 import { canLoadAuthGuard } from "./services/can-load-auth.guard";
 import { CustomPreloadingStrategy } from "./services/custom-preloading.strategy";
+import { ChatComponent } from "./chat/chat.component";
 
 const routes: Routes = [
   {
@@ -33,6 +34,13 @@ const routes: Routes = [
   {
     path: "about",
     component: AboutComponent,
+  },
+  // route to place component in secondary outlet
+  {
+    path: "helpdesk-chat",
+    component: ChatComponent,
+    //name of secondary outlet
+    outlet: "chat",
   },
   {
     path: "**",
