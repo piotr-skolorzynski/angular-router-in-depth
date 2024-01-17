06fdc0c51dab50f1828aa78c9362377b8a026f27
piotr-skolorzynski <p.skolorzynski@gmail.com>
2024-01-17 22:08:53 +0100

    router extra options

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -52,6 +52,15 @@ const routes: Routes = [
   imports: [
     RouterModule.forRoot(routes, {
       preloadingStrategy: CustomPreloadingStrategy,
+      //usefull to debug, display a lot of information in console
+      // enableTracing: true,
+      // let to use routes with #
+      // useHash: true,
+      //automatically scrolls to chosen position of page if is e.g. top By default is set to disabled
+      // enabled - if we navigate first time to page it scrolls to the top but if we back it scrolls to last position on page - it is browers behave
+      scrollPositionRestoration: "enabled",
+      //it is worth to set like that because the you do not have to take some route params from parent but it is inherited in child paramMap
+      paramsInheritanceStrategy: "always",
     }),
   ],
   exports: [RouterModule],
